087345180abae81e375aa94e630389cfafab627f
Youness-dev7 <hammou2000@hotmail.it>
2025-04-28 00:10:39 +0200

    input and button design system

--- hello-next/src/app/page.tsx
+++ hello-next/src/app/page.tsx
@@ -1,9 +1,16 @@
+'use client'
 import Image from "next/image";
-
+import { useEffect } from 'react';
 export default function Home() {
+  useEffect(() => {
+    import('@youness-dev7/my-design-system');
+  }, []);
   return (
-    <div className="flex min-h-screen items-center justify-center bg-gray-100">
-    <p className="text-4xl font-bold text-blue-600">Ciao sono la home</p>
+    <div className="flex flex-col min-h-screen items-center justify-center bg-gray-100">
+      <p className="text-4xl font-bold text-blue-600">Input e buttone del design system</p>
+      <my-input></my-input>
+      <my-button label="Invia"></my-button>
     </div>
+  
   );
 }
